95be3c3acec29d71bb4c026738c8b9ea6ae7f441
Andrew Shen <55771349+azshen-umich@users.noreply.github.com>
2021-09-30 16:54:48 -0400

    commit

--- mostActiveCookies.cpp
+++ mostActiveCookies.cpp
@@ -5,22 +5,25 @@
 #include <string>
 #include "mostActiveCookies.h"
 
-
-
-
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     mostActiveCookies cookie;
     cookie.readCmdLine(argc, argv);
-    exit (0);
+    exit(0);
 }
 
 void mostActiveCookies::readCmdLine(int argc, char *argv[])
 {
-
+    filename = argv[0];
+    if (argv[1] == "-d")
+    {
+        utc = true;
+    }
+    date = convertDate(argv[2]);
 }
 
-
-
-
+int convertDate(string stringDate)
+{
+}
--- mostActiveCookies.h
+++ mostActiveCookies.h
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <getopt.h>
+#include <string>
+
+using namespace std;
+
 class mostActiveCookies
 {
-    public:
-        void readCmdLine(int argc, char *argv[]);
-        void findActiveCookie();
+public:
+    //reads in command line arguments
+    void readCmdLine(int argc, char *argv[]);
+    //finds most active cookies from given day
+    void findActiveCookie();
+    //removes the dashes in the date and coverts it to a string to int
+    int convertDate(string stringDate);
+
+private:
+    string filename;  //name of the cookies log file
+    bool utc = false; // -d utc time zone parameter
+    int date;         //the specified date from command line
 };
