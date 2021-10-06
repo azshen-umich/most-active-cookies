3a3065fb7588a9cfe2b9aa18b8c90f06b641c723
Andrew Shen <azshen23@gmail.com>
2021-10-06 13:46:35 -0400

    fixed bugs

--- most_active_cookie.cpp
+++ most_active_cookie.cpp
@@ -4,38 +4,66 @@
 #include <algorithm>
 #include <getopt.h>
 #include <string>
+#include <fstream>
 #include "mostActiveCookies.h"
 #include <unordered_map>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+class mostActiveCookies
 {
-    mostActiveCookies cookie;
-    cookie.readCmdLine(argc, argv);
-    cookie.readLog();
-    cookie.findActiveCookie();
-    exit(0);
-}
+public:
+    struct CookieInfo
+    {
+        string cookie;   //string of the cookie from the log
+        int cookieCount; //number of cookies that appear on specific date
+
+        CookieInfo(string cookies, int numCookie) : cookie(cookies), cookieCount(numCookie) {}
+    };
+    //functor to compare which cookies have higher counts
+    struct compareCookies
+    {
+        bool operator()(const CookieInfo cookie1, const CookieInfo cookie2)
+        {
+            return cookie1.cookieCount > cookie2.cookieCount;
+        }
+    };
+    //reads in command line arguments
+    void readCmdLine(int argc, char *argv[]);
+    //finds most active cookies from given day
+    void findActiveCookie();
+    //removes the dashes in the date and coverts it to a string to int
+    int convertDate(string stringDate);
+    //read the cookie log and initialize data structures with the log info
+    void readLog();
+
+private:
+    bool utc = false;                           // -d utc time zone parameter
+    int dateIn;                                 //the specified date from command line
+    unordered_map<int, vector<CookieInfo>> map; //key value pair with date and list of cookies on that date
+    ifstream infile;                            //cookie logfile
+};
 
 void mostActiveCookies::readCmdLine(int argc, char *argv[])
 {
-    filename = argv[0];
-    if (argv[1] == "-d")
+    infile.open(argv[1]);
+    streambuf *cinbuf = cin.rdbuf();
+    cin.rdbuf(infile.rdbuf());
+
+    if (argv[2] == "-d")
     {
         utc = true;
     }
-    dateIn = convertDate(argv[2]);
+    dateIn = convertDate(argv[3]);
 }
 
 int mostActiveCookies::convertDate(string stringDate)
 {
-    cout << stringDate;
     if (stringDate.length() == 10) //check if the stringDate is from command line
     {
         //remove the dashes from the date
         stringDate.erase(stringDate.begin() + 4);
-        stringDate.erase(stringDate.begin() + 7);
+        stringDate.erase(stringDate.begin() + 6);
     }
     //convert to integer
     return stoi(stringDate);
@@ -43,23 +71,24 @@ int mostActiveCookies::convertDate(string stringDate)
 
 void mostActiveCookies::readLog()
 {
-    string cookieInput, dateInput, trash;
+    string cookieInput, dateInput, trash, input;
     int date, time;
 
     getline(cin, trash); //throw out first line
 
-    while (cin >> cookieInput)
+    while (cin >> input)
     {
-        cin >> dateInput;
+        cookieInput = input.substr(0, input.find(","));
+        dateInput = input.substr(input.find(",") + 1, input.length());
 
         dateInput.erase(dateInput.begin() + 4);
-        dateInput.erase(dateInput.begin() + 7);
-        dateInput.erase(dateInput.begin() + 10, dateInput.end());
+        dateInput.erase(dateInput.begin() + 6);
+        dateInput.erase(dateInput.begin() + 8, dateInput.end());
         date = stoi(dateInput);
 
         pair<int, vector<CookieInfo>> temp;
 
-        if (map.find(date) != map.end()) //check if date key exists, add cookie to that date
+        if (map.find(date) == map.end()) //check if date key exists, add cookie to that date
         {
             auto pred = [cookieInput](CookieInfo cookieData)
             {
@@ -82,6 +111,7 @@ void mostActiveCookies::readLog()
             temp.second.push_back(CookieInfo(cookieInput, 1));
         }
     }
+    infile.close();
 }
 
 void mostActiveCookies::findActiveCookie()
@@ -101,4 +131,13 @@ void mostActiveCookies::findActiveCookie()
             return;
         }
     }
+}
+
+int main(int argc, char *argv[])
+{
+    mostActiveCookies cookie;
+    cookie.readCmdLine(argc, argv);
+    cookie.readLog();
+    cookie.findActiveCookie();
+    exit(0);
 }
