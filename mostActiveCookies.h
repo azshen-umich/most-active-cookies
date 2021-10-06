3ce715343aa702a7970e79f6174c54e9e3bd2227
Andrew Shen <azshen23@gmail.com>
2021-10-06 14:47:50 -0400

    separate to .h file

--- mostActiveCookies.h
+++ mostActiveCookies.h
@@ -8,3 +8,37 @@
 #include <queue>
 
 using namespace std;
+
+class mostActiveCookies
+{
+public:
+    struct CookieInfo
+    {
+        string cookie;   //string of the cookie from the log
+        int cookieCount; //number of cookies that appear on specific date
+
+        CookieInfo(string cookies, int numCookie) : cookie(cookies), cookieCount(numCookie) {}
+    };
+    //functor to compare which cookies have higher counts
+    struct compareCookies
+    {
+        bool operator()(const CookieInfo cookie1, const CookieInfo cookie2)
+        {
+            return cookie1.cookieCount > cookie2.cookieCount;
+        }
+    };
+    //reads in command line arguments
+    void readCmdLine(int argc, char *argv[]);
+    //finds most active cookies from given day
+    void findActiveCookie();
+    //removes the dashes in the date and coverts it to a string to int
+    int convertDate(string stringDate);
+    //read the cookie log and initialize data structures with the log info
+    void readLog();
+
+private:
+    bool utc = false;                           // -d utc time zone parameter
+    int dateIn;                                 //the specified date from command line
+    unordered_map<int, vector<CookieInfo>> map; //key value pair with date and list of cookies on that date
+    ifstream infile;                            //cookie logfile
+};
--- most_active_cookie.cpp
+++ most_active_cookie.cpp
@@ -10,40 +10,6 @@
 
 using namespace std;
 
-class mostActiveCookies
-{
-public:
-    struct CookieInfo
-    {
-        string cookie;   //string of the cookie from the log
-        int cookieCount; //number of cookies that appear on specific date
-
-        CookieInfo(string cookies, int numCookie) : cookie(cookies), cookieCount(numCookie) {}
-    };
-    //functor to compare which cookies have higher counts
-    struct compareCookies
-    {
-        bool operator()(const CookieInfo cookie1, const CookieInfo cookie2)
-        {
-            return cookie1.cookieCount > cookie2.cookieCount;
-        }
-    };
-    //reads in command line arguments
-    void readCmdLine(int argc, char *argv[]);
-    //finds most active cookies from given day
-    void findActiveCookie();
-    //removes the dashes in the date and coverts it to a string to int
-    int convertDate(string stringDate);
-    //read the cookie log and initialize data structures with the log info
-    void readLog();
-
-private:
-    bool utc = false;                           // -d utc time zone parameter
-    int dateIn;                                 //the specified date from command line
-    unordered_map<int, vector<CookieInfo>> map; //key value pair with date and list of cookies on that date
-    ifstream infile;                            //cookie logfile
-};
-
 void mostActiveCookies::readCmdLine(int argc, char *argv[])
 {
     infile.open(argv[1]);
@@ -108,8 +74,6 @@ void mostActiveCookies::readLog()
         else //if date key DNE, then insert a new key value pair.
         {
             map[date].push_back(CookieInfo(cookieInput, 1));
-            //temp.first = date;
-            //temp.second.push_back(CookieInfo(cookieInput, 1));
         }
     }
     infile.close();
